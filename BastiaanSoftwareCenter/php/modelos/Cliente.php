634bb6077e2a169939341574ea4b794b28cb552f
Daniela <Daniela@192.168.1.64>
2017-10-17 13:24:34 -0500

    Modificacion para agregar parametros

--- BastiaanSoftwareCenter/php/modelos/Cliente.php
+++ BastiaanSoftwareCenter/php/modelos/Cliente.php
@@ -5,6 +5,21 @@ class Cliente
 {
     public $id;
     public $nombre;
+    public $nombreSegundo;
     public $apellidoPaterno;
+    public $apellidoMaterno;
+    public $nombreCompleto;
+    public $rfc;
+    public $nss;
+    public $curp;
+    public $cpId;
+    public $numExt;
+    public $numInt;
+    public $calle;
+    public $colonia;
+    public $estado;
+    public $pais;
+    public $direccion;
+    public $correoElectronico;
 }
 
