6c84665f6d3668ec7e81813419476d5442714e8c
Alan Bazan <alanbazan@hotmail.com>
2017-10-17 12:22:02 -0500

    -modificacion repositorio js de clientes

--- BastiaanSoftwareCenter/js/repositorios/clientes_repositorio.js
+++ BastiaanSoftwareCenter/js/repositorios/clientes_repositorio.js
@@ -5,10 +5,10 @@ class ClientesRepositorio
 		
 	}
 	
-	consultar(CallBackContext,CallBackFunction, nombreCompleto)
+	consultar(contexto,functionRetorno, nombreCompleto)
 	{		
-		this.CallBackContext = CallBackContext;
-		this.CallBackFunction = CallBackFunction;
+		this.contexto = contexto;
+		this.functionRetorno = functionRetorno;
 		
 		var parametros;
 		parametros = "accion=consultar";
@@ -16,8 +16,7 @@ class ClientesRepositorio
 		
 		
 		var contextHandler = new AjaxContextHandler();
-		var host = window.location.origin + "/BastiaanSoftwareCenter";
-		//var ai = new Ajaxv2("http://127.0.0.1:7000/BastiaanSoftwareCenter/php/repositorios/clientes.php", this, this.consultarResultado, "POST", parametros, contextHandler);		
+		var host = window.location.origin + "/BastiaanSoftwareCenter";	
 		var ai = new Ajaxv2(host +"/php/repositorios/Clientes.php", this, this.consultarResultado, "POST", parametros, contextHandler);		
 		contextHandler.AddAjaxv2Object(ai); 		
 		ai.GetPost(true);
@@ -26,7 +25,7 @@ class ClientesRepositorio
 	consultarResultado(resultado)
 	{
 		var datos = JSON.parse(resultado);
-		this.CallBackFunction.call(this.CallBackContext,JSON.parse(resultado));
+		this.functionRetorno.call(this.contexto,JSON.parse(resultado));
 	}
 	
 
