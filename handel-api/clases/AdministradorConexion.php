e944c70cbd4d024882feb2a5bd564b053e070a7d
Alan Bazan <alanbazan@hotmail.com>
2017-10-31 17:15:58 -0600

    -uploaded

---
+++ handel-api/clases/AdministradorConexion.php
@@ -0,0 +1,25 @@
+<?php
+namespace clases;
+use mysqli;
+
+
+class AdministradorConexion
+{
+    private $servidor = "alanbazan.com.mx";
+    private	$basedatos = "handel_db";
+    private	$usuario = "handelscenet";
+    private	$contrasena ="NOem0307!";
+    public function abrir()
+    {
+        return new mysqli($this->servidor,$this->usuario,$this->contrasena,$this->basedatos);
+    }
+    
+    public function cerrar($connection)
+    {
+        if($connection)
+            //mysqli_close($connection);
+            $connection->close();
+    }
+    
+}
+
---
+++ handel-api/clases/Resultado.php
@@ -0,0 +1,15 @@
+<?php
+namespace clases;
+
+class Resultado
+{   
+    public function __construct()
+    {
+        $this->mensajeError = "";
+        
+    }
+    public $mensajeError;
+    public $valor;
+}
+  
+
